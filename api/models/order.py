7913849f6c6eb6acbc4ffc41638ef04d9c937737
jzamora5 <jzamora_5@yahoo.com>
2021-03-23 11:20:42 -0500

    Add last_payment_date to orders

--- api/models/order.py
+++ api/models/order.py
@@ -2,9 +2,13 @@
 
 from api.models import Base
 from api.models.base_model import BaseModel
-from api.models.shipping import Shipping
+from api.models.payment import Payment
 from sqlalchemy import Column, String, ForeignKey, Float, Boolean
 from sqlalchemy.orm import relationship
+from api.utils.sort import mergeSort
+
+
+TIME_FORMAT = "%Y-%m-%dT%H:%M:%S.%f"
 
 
 class Order(BaseModel, Base):
@@ -30,6 +34,7 @@ class Order(BaseModel, Base):
         new_dict = super().to_dict()
         shipping = self.shipping
         user = self.user
+        payments = self.payments
 
         if shipping:
             new_dict["shipping_info"] = shipping.to_dict()
@@ -39,9 +44,24 @@ class Order(BaseModel, Base):
         if user:
             user_dict = user.to_dict()
             user_dict.pop("orders", None)
+            new_dict["customer_id"] = user_dict.pop("id")
+            new_dict["customer_name"] = user_dict.pop("name")
+
             new_dict["user_information"] = user_dict
+
         else:
             new_dict["user_information"] = "Not Available"
+
+        list_payments = []
+        for payment in payments:
+            list_payments.append(payment.to_dict())
+
+        if list_payments:
+            mergeSort(list_payments, ["created_at"])
+            new_dict["last_payment_date"] = payments[-1].created_at.strftime(
+                TIME_FORMAT)
+        else:
+            new_dict["last_payment_date"] = "Not Available"
 
         new_dict.pop("user", None)
         new_dict.pop("shipping", None)
